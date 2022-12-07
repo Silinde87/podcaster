4cf473100793672fd949200e8112b25d6192c982
Pau Rodriguez <paurodriguezmolina@gmail.com>
2022-12-07 10:17:55 +0100

    feat: Add API interfaces

--- src/services/Podcast.service.ts
+++ src/services/Podcast.service.ts
@@ -1,21 +1,22 @@
 import http from './http-services/http';
 import { API } from 'utils/constants/api.constants';
+import { IPodcastDetails } from 'utils/interfaces/api/podcast_details.interface';
+import { IPodcasts } from 'utils/interfaces/api/podcasts.interface';
 
-// TODO: Set proper response types
 const PodcastService = () => {
-  const getPodcasts = async (): Promise<any> => {
+  const getPodcasts = async (): Promise<IPodcasts> => {
     return http
       .GET({ path: API.PODCASTS })
-      .then(response => response)
+      .then(response => response as IPodcasts)
       .catch(error => {
         throw error;
       });
   };
 
-  const getPodcastDetails = async (podcastId: string): Promise<any> => {
+  const getPodcastDetails = async (podcastId: string): Promise<IPodcastDetails> => {
     return http
       .GET({ path: API.PODCAST_DETAIL(podcastId) })
-      .then(response => response)
+      .then(response => response as IPodcastDetails)
       .catch(error => {
         throw error;
       });
